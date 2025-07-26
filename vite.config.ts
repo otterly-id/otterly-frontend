9211b44c88bc6aa2671e97bfefef09ad81854a4e
Samuel Yuma K <samuelyuma.117@gmail.com>
2025-07-26 18:37:54 +0700

    chore: shadcn init and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    host: true,
+    port: 3000,
+  },
+  preview: {
+    host: true,
+    port: 3000,
+  },
 });
