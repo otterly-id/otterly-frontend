1077fafe58f27e40477dd9aa619a1b5a6fafee0b
harvdt <harviandito20@gmail.com>
2025-07-30 19:30:06 +0700

    feat: configure netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
   server: {
     host: true,
     port: 3000,
